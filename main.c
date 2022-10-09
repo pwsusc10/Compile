c36f1953f43e888b11f314b3c5aff92d60b26daa
Sungpyo <pwsusc10@naver.com>
2022-10-09 19:43:21 +0900

    lexical Analyzer

--- main.c
+++ main.c
@@ -22,11 +22,13 @@ int main(void)
     // file line number
     int f_line = 0;
 
-    // symbol table과 buffer table의 인덱스
-    int symbol_idx = 1;
+    // id table과 buffer table의 인덱스
+    int id_idx = 1;
+    int str_idx = 1;
     int buffer_idx = 0;
     int string_idx = 0;
     int token_idx = 0;
+    int cnt = 0;
 
     char buffer[max_len];
     char string[max_len];
@@ -97,16 +99,32 @@ int main(void)
                 }
                 else
                 {
-                    // id code
-                    token_list[token_idx].code_num = 1;
-                    token_list[token_idx].idx = symbol_idx;
-                    strcpy(token_list[token_idx].name, "ID");
-                    strcpy(token_list[token_idx].data, string);
-                    token_idx++;
-                    symbol_idx++;
+                    // 중복 체크
+                    for (cnt = 0; cnt < token_idx; cnt++)
+                    {
+                        if ((token_list[cnt].code_num == 1) && strcmp(token_list[cnt].data, string) == 0)
+                            break;
+                    }
+                    if (cnt == token_idx)
+                    {
+                        // id code
+                        token_list[token_idx].code_num = 1;
+                        token_list[token_idx].idx = id_idx;
+                        strcpy(token_list[token_idx].name, "ID");
+                        if (strlen(string) > 10)
+                            string[10] = '\0';
+                        strcpy(token_list[token_idx].data, string);
+                        token_idx++;
+                        id_idx++;
 
-                    memset(string, 0, max_len * sizeof(char));
-                    string_idx = 0;
+                        memset(string, 0, max_len * sizeof(char));
+                        string_idx = 0;
+                    }
+                    else
+                    {
+                        memset(string, 0, max_len * sizeof(char));
+                        string_idx = 0;
+                    }
                 }
             }
 
@@ -308,8 +326,7 @@ int main(void)
                 memset(string, 0, max_len * sizeof(char));
                 string_idx = 0;
             }
-
-            // 정수 실수 처리
+            // 3) INT, FLOAT
             if ((buffer[i] >= '0' && buffer[i] <= '9') || buffer[i] == '.')
             {
                 // 00, 01, 02과 같이 앞에 0이 오는 것에 대한 오류 처리.
@@ -429,9 +446,112 @@ int main(void)
                 memset(string, 0, max_len * sizeof(char));
                 string_idx = 0;
             }
+            // 4) STRING
+            if (buffer[i] == '"')
+            {
+                string[string_idx] = buffer[i];
+                string_idx++;
+                i++;
+                // string 끝이 나올때 까지 문자열에 담기
+                while (1)
+                {
+                    if (buffer[i] == '\n')
+                    {
+                        string[string_idx] = '\\';
+                        string_idx++;
+                        string[string_idx] = 'n';
+                        string_idx++;
+                        fgets(buffer, 1000, stdin);
+                        i = 0;
+                    }
+                    else if (buffer[i] == '\t')
+                    {
+                        string[string_idx] = '\\';
+                        string_idx++;
+                        string[string_idx] = 't';
+                        string_idx++;
+                        i++;
+                    }
+                    else if (buffer[i] == '\\') // \에 대한 오류 처리
+                    {
+                        token_list[token_idx].code_num = 0;
+                        token_list[token_idx].idx = f_line;
+                        strcpy(token_list[token_idx].data, "\\");
+                        token_idx++;
+                        i++;
+                    }
+                    else
+                    {
+                        string[string_idx] = buffer[i];
+                        string_idx++;
+                        if (buffer[i] == '"')
+                        {
+                            i++;
+                            break;
+                        }
+                        i++;
+                    }
+                }
+                // 중복 체크
+                for (cnt = 0; cnt < token_idx; cnt++)
+                {
+                    if ((token_list[cnt].code_num == 5) && strcmp(token_list[cnt].data, string) == 0)
+                        break;
+                }
+                if (cnt == token_idx)
+                {
+                    // string code
+                    token_list[token_idx].code_num = 5;
+                    token_list[token_idx].idx = str_idx;
+                    strcpy(token_list[token_idx].name, "STRING");
+                    strcpy(token_list[token_idx].data, string);
+                    token_idx++;
+                    str_idx++;
+
+                    memset(string, 0, max_len * sizeof(char));
+                    string_idx = 0;
+                }
+                else
+                {
+                    memset(string, 0, max_len * sizeof(char));
+                    string_idx = 0;
+                }
+            }
+            // 5) SEMICOLON
+            if (buffer[i] == ';')
+            {
+                token_list[token_idx].code_num = 2;
+                strcpy(token_list[token_idx].name, "SEMICOLON");
+                strcpy(token_list[token_idx].data, ";");
+                token_idx++;
+                i++;
+            }
+            // 문장 마지막에 ';'이 없을 경우 에러처리
+            if (i == (strlen(buffer) - 1) && buffer[i - 1] != ';')
+            {
+                // error
+                token_list[token_idx].code_num = 0;
+                token_list[token_idx].idx = f_line;
+                strcpy(token_list[token_idx].data, "';'이 없습니다.");
+                token_idx++;
+            }
         }
     } while (fgets(buffer, 1000, stdin) != NULL);
 
+    // id table
+    printf("index\tids\n");
+    for (int i = 0; i < token_idx; i++)
+    {
+        if (token_list[i].code_num == 1)
+            printf("%d\t\"%s\"\n", token_list[i].idx, token_list[i].data);
+    }
+    // String table
+    printf("index\tstrings\n");
+    for (int i = 0; i < token_idx; i++)
+    {
+        if (token_list[i].code_num == 5)
+            printf("%d\t%s\n", token_list[i].idx, token_list[i].data);
+    }
     // token 출력 처리
     for (int i = 0; i < token_idx; i++)
     {
